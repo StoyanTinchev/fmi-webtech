ecf1178efef8d4ccd6603d755e212869a0341932
StoyanTinchev <st8tinchev@gmail.com>
2025-03-05 11:13:25 +0200

    webtech todos project

---
+++ week2/todo.js
@@ -0,0 +1,7 @@
+function addTodo() {
+    const input = document.querySelector('input')
+    const ul = document.querySelector('ul')
+    const li = document.createElement('li')
+    li.textContent = input.value
+    ul.appendChild(li)
+}

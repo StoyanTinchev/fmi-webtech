75e07261cec73cd1ed60ecc28ede644500570917
StoyanTinchev <st8tinchev@gmail.com>
2025-03-05 11:54:56 +0200

    add remove todos functionality

--- week2/todo.js
+++ week2/todo.js
@@ -2,6 +2,7 @@ function addTodo() {
     const input = document.querySelector('input')
     const ul = document.querySelector('ul')
     const li = document.createElement('li')
+    li.onclick = () => ul.removeChild(li)
     li.textContent = input.value
     ul.appendChild(li)
 }
